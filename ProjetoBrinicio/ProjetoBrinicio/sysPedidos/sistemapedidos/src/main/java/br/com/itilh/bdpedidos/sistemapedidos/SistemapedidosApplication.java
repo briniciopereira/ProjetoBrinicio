1acd9ccd099eed00cdfa98169e157a462338457b
briniciopereira <brinicio13@gmail.com>
2024-07-08 21:40:44 -0300

    correções

--- ProjetoBrinicio/ProjetoBrinicio/sysPedidos/sistemapedidos/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/SistemapedidosApplication.java
+++ ProjetoBrinicio/ProjetoBrinicio/sysPedidos/sistemapedidos/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/SistemapedidosApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 @SpringBootApplication
 public class SistemapedidosApplication {
@@ -20,6 +21,10 @@ public class SistemapedidosApplication {
             public void addViewControllers(ViewControllerRegistry registry) {
                 registry.addViewController( "/" )
                         .setViewName("forward:/app/index.html");
+            }
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
             }
         };
     
